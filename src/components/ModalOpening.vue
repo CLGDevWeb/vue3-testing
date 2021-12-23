e87ddd20c0912690e56b5f649521d023b0a68f0d
CLGDevWeb <clgdevweb@gmail.com>
2021-12-23 14:54:55 +0100

    first free test, create component

---
+++ src/components/ModalOpening.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button @click="toggleModal" type="button">Open modal</button>
+
+        <div v-if="modalIsOpen" class="modal">
+            <p>
+                Modal is open !
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+    setup () {
+        const modalIsOpen = ref(false)
+
+        return {
+            toggleModal: () => modalIsOpen.value = !modalIsOpen.value
+        }
+    }
+}
+</script>
